504cadbe5314597959e12163045f09897686c985
Owl <66283153+maryjng94@users.noreply.github.com>
2020-08-28 21:18:00 -0400

    major overhaul

--- run.py
+++ run.py
@@ -1,36 +1,41 @@
 from PIL import Image
+from numpy import reshape
 
-#with Image.open("cat.jpg") as im:
-#	im = im.convert("L")
+charList =  `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$
 
-ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
-#255/11 for range
+def resize(picture, new_width = 25):
+	width, height = picture.size
+	ratio = width//height
+	new_height = new_width//ratio
 
-def resize(picture, OG_width):
-	
-
-def pixel_to_ASCII(picture):
-
+	return new_width, new_height
 
 
+#resize original pic. takes new_width and calculates new_height using the pic's width/height ratio.
+
+def convGray(picture):
+	with Image.open(picture) as im:
+		im = im.convert("L")
 
 
+	# not needed prolly. numpy.reshape(pixels, (width, height))
+	#graypixels list
 
-def convLumi(picture):
-	im = Image.open(picture)
-	
-	#get list of RGB tuples for each pixel
+def to_ASCII(picture):
+	convGray(picture)
+
 	pixels = list(im.getdata())
-	
-	#get dimensions of pic
+
 	width, height = im.size
-	
-	pixelList = []
-	for x in range(width):
-		for y in range(length):
-			pix = pixels[x, y]
-			#convert to ASCII char
-			lumi = (0.3 * pix[0]) + (0.59 * pix[1]) + (0.11 * pix[2])
-			pixelList.append(lumi)
-		
-	print(pixelList)
+
+	newpic = []
+	for pix in graypixels:
+		newpic.append(charList[pix // 4])
+
+	numpy.reshape(newpic, (width, height))
+	print(newpic)
+
+# each pixel has a value from 0 - 255. Floor by 4 and assign to ASCII char.
+
+
+convGray("sadcat.PNG")
